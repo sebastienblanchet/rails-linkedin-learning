ea2d8651bd000b7951249ac458f9675cf9409a98
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-04-11 16:16:46 -0400

    Router links using controllers

--- src/app/controllers/demo_controller.rb
+++ src/app/controllers/demo_controller.rb
@@ -7,7 +7,6 @@ class DemoController < ApplicationController
   end
 
   def hello
-    # this is an instance variable ==> accessible in the view
     @array = [1,2,3,4,5]
     # render('hello')
     # redirect_to(:controller => 'demo', :action => 'index')
