eb0f88ecdcbcaa33c1507e330f8a7ee9e21154e1
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-04-11 18:10:07 -0400

    Add submit form route, up to  ActiveModel::ForbiddenAttributesError

--- src/app/controllers/subjects_controller.rb
+++ src/app/controllers/subjects_controller.rb
@@ -13,6 +13,16 @@ class SubjectsController < ApplicationController
   end
 
   def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(params[:subject])
+    # Save the object
+    if @subject.save
+      # If save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def edit
