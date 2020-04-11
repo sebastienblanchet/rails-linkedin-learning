686fdd377834dc05a64278a42d71b619c731fbbd
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-04-11 16:01:04 -0400

    Render view template in controller actions

--- src/app/controllers/demo_controller.rb
+++ src/app/controllers/demo_controller.rb
@@ -1,7 +1,15 @@
 class DemoController < ApplicationController
 
   layout false
-  
+
   def index
+    render('hello')
   end
+
+  def hello
+    # no need for file exention
+    # render(:template => 'demo/hello')
+    render('index')
+  end
+
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # this is the "/" route root
+
   root 'demo#index'
-  # default route
+
   get 'demo/index'
-  # match routes basic matches
-  get ':controller(/:action(/:id))'
-  # ex GET /student/edit/52 ==> student controller, edit action id 52
+  get 'demo/hello'
+
+  #default route
+  #get ':controller(/:action(/:id))'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
