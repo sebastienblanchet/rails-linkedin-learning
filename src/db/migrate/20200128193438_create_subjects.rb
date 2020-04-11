5bfabe4a52d531fbf087c2f01a1873d4f362fa37
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-04-11 17:46:09 -0400

    Ressoureful URL helpers

--- src/db/migrate/20200128193438_create_subjects.rb
+++ src/db/migrate/20200128193438_create_subjects.rb
@@ -1,12 +1,15 @@
 class CreateSubjects < ActiveRecord::Migration[6.0]
+  def up
 
-  def change
-    create_table :subjects do |t|
-      t.string :name
-      t.integer :position
-      t.boolean :visible
-      t.timestamps
-    end
+      create_table :subjects do |t|
+        t.string :name
+        t.integer :position
+        t.boolean :visible
+        t.timestamps
+      end
+  end
+
+  def down
   end
 
 end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_193447) do
+ActiveRecord::Schema.define(version: 2020_04_11_205232) do
 
-  create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
+  create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "subject_id"
     t.string "name"
     t.string "permalink"
@@ -22,22 +22,6 @@ ActiveRecord::Schema.define(version: 2020_01_28_193447) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["subject_id"], name: "index_pages_on_subject_id"
-  end
-
-  create_table "subjects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
-    t.string "name"
-    t.integer "position"
-    t.boolean "visible"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
