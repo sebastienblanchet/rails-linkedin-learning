67a784477dd1c7f19b4a9913d1f5a3241a27f350
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-04-11 13:57:27 -0400

    Overview of basic route types

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # this is the "/" route root
+  root 'demo#index'
+  # default route
   get 'demo/index'
+  # match routes basic matches
+  get ':controller(/:action(/:id))'
+  # ex GET /student/edit/52 ==> student controller, edit action id 52
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
