15cb9d568ad53125c86095eb418c63377e0f1569
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-04-11 16:03:54 -0400

    Redirect controller action

--- src/app/controllers/demo_controller.rb
+++ src/app/controllers/demo_controller.rb
@@ -3,13 +3,19 @@ class DemoController < ApplicationController
   layout false
 
   def index
-    render('hello')
+    # render('index')
   end
 
   def hello
-    # no need for file exention
-    # render(:template => 'demo/hello')
-    render('index')
+    # does this automatically
+    # render('hello')
+
+    # pass in controller
+    # goes to routes.rb and determine
+    redirect_to(:controller => 'demo', :action => 'index')
+    # (:action => 'index')
+    # could aslso
+    # redirect_to('https://github.com')
   end
 
 end
